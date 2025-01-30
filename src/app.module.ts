71ad8dc6028c3746b33b8314a113eef63c78f8c3
gaba-teixeira <gaba.andrade@outlook.com>
2025-01-30 13:38:43 -0300

    :sparkles: Criar CRUD do Recurso Usuario

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuarioModule } from './Usuario/usuario.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: '', // Cadastrar o nome do BD
-    entities: [], // Cadastrar as Classes Entities, para que o 
-    // TypeORM possa gerar as tabelas correspondentes no BD
-    synchronize: true,
-    logging: true
-  }),
-  // Inserir nome da classe module de cada entidade (ex.: PostagemModule)
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_food_delivery_app',
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+    }),
+  UsuarioModule
   ],
   controllers: [],
   providers: [],
