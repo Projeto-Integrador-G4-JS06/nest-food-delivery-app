355170e32545b4a7b0617117ed546a8b18676a12
Fernando Lana <loslanas@gmail.com>
2025-01-30 12:53:10 -0300

    :sparkles: Criar Categoria CRUD

---
+++ src/categoria/entities/categoria.entity.ts
@@ -0,0 +1,35 @@
+import { Transform, TransformFnParams } from "class-transformer";
+import { IsNotEmpty } from "class-validator";
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+
+@Entity({name: "tb_categorias"})
+export class Categoria {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Transform(({ value }: TransformFnParams) => value?.trim())
+    @IsNotEmpty()
+    @Column({length: 255, nullable: false})
+    nome_categoria: string;
+
+    @Column({ type: 'boolean', nullable: false, default: true })
+    @IsNotEmpty()
+    status: boolean;
+
+    @Transform(({ value }: TransformFnParams) => value?.trim())
+    @IsNotEmpty()
+    @Column({length: 255, nullable: false})
+    descricao: string;
+
+    @CreateDateColumn()
+    criado_em: Date;
+    
+    @UpdateDateColumn()
+    atualizado_em: Date;
+
+    // @OneToMany(() => Produto, (produto) => produto.categoria)
+    // produto: Produto[];
+    
+}
