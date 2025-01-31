abe0f1e1635443b7bc8a03e92a078e069d5ac297
alanbrunoscience <alanbrunomath@gmail.com>
2025-01-31 14:44:03 -0300

    :bug: Update format data to PostgreSQL

--- src/produto/services/produto.service.ts
+++ src/produto/services/produto.service.ts
@@ -73,7 +73,7 @@ export class ProdutoService {
         'p.nutri_score',
         'p.criado_em',
         'p.atualizado_em',
-        "CASE WHEN p.status = 1 THEN 'Disponível' WHEN p.status = 0 THEN 'Indisponível' END AS status_prod",
+        "CASE WHEN p.status = true THEN 'Disponível' WHEN p.status = false THEN 'Indisponível' END AS status_prod",
       ])
       .innerJoin('p.usuario', 'u')
       .innerJoin('p.categoria', 'c')
