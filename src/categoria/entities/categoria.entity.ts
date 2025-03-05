f77ec3be49dfcc508623146148fc54f0f5e9f8d8
Samuel <samuka2568@gmail.com>
2025-03-05 10:26:38 -0300

    :sparkles: Definir campos redeploy

--- src/categoria/entities/categoria.entity.ts
+++ src/categoria/entities/categoria.entity.ts
@@ -1,43 +1,47 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-import { Transform, TransformFnParams } from "class-transformer";
-import { IsNotEmpty } from "class-validator";
-import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-import { Produto } from "../../produto/entities/produto.entity";
+import { Transform, TransformFnParams } from 'class-transformer';
+import { IsNotEmpty } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Produto } from '../../produto/entities/produto.entity';
 
-
-@Entity({name: "tb_categorias"})
+@Entity({ name: 'tb_categorias' })
 export class Categoria {
+  @PrimaryGeneratedColumn()
+  @ApiProperty()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    @ApiProperty()
-    id: number;
+  @Transform(({ value }: TransformFnParams) => value?.trim())
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  @ApiProperty()
+  nome_categoria: string;
 
-    @Transform(({ value }: TransformFnParams) => value?.trim())
-    @IsNotEmpty()
-    @Column({length: 255, nullable: false})
-    @ApiProperty()
-    nome_categoria: string;
+  @Column({ type: 'boolean', nullable: true, default: true })
+  @IsNotEmpty()
+  @ApiProperty()
+  status: boolean;
 
-    @Column({ type: 'boolean', nullable: false, default: true })
-    @IsNotEmpty()
-    @ApiProperty()
-    status: boolean;
+  @Transform(({ value }: TransformFnParams) => value?.trim())
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  @ApiProperty()
+  descricao: string;
 
-    @Transform(({ value }: TransformFnParams) => value?.trim())
-    @IsNotEmpty()
-    @Column({length: 255, nullable: false})
-    @ApiProperty()
-    descricao: string;
+  @CreateDateColumn()
+  criado_em: Date;
 
-    @CreateDateColumn()
-    criado_em: Date;
-    
-    @UpdateDateColumn()
-    atualizado_em: Date;
+  @UpdateDateColumn()
+  atualizado_em: Date;
 
-    @ApiProperty()
-    @OneToMany(() => Produto, (produto) => produto.categoria)
-    produto: Produto[];
-    
+  @ApiProperty()
+  @OneToMany(() => Produto, (produto) => produto.categoria)
+  produto: Produto[];
 }
